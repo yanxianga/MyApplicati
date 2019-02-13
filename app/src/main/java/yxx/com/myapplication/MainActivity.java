f15ba970a532f3d2acdf641ed4a83d6a65e37618
yanxianga <2622785474@qq.com>
2019-02-13 20:46:05 +0800

    "晚上练习"

--- app/src/main/java/yxx/com/myapplication/MainActivity.java
+++ app/src/main/java/yxx/com/myapplication/MainActivity.java
@@ -2,12 +2,60 @@ package yxx.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    String path = "http://result.eolinker.com/iYXEPGn4e9c6dafce6e5cdd23287d2bb136ee7e9194d3e9?uri=one";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("yxx","qqq");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String data = getData();
+            }
+        }).start();
+
+    }
+
+    private String getData() {
+
+        try {
+            URL url = new URL(path);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            if(httpURLConnection.getResponseCode() == 200){
+                InputStream inputStream = httpURLConnection.getInputStream();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+                byte[] bytes = new byte[1024];
+
+                int length = 0;
+
+                while ((length = inputStream.read(bytes)) != -1){
+                    outputStream.write(bytes,0,length);
+                }
+
+                outputStream.toString();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return "";
     }
 }
